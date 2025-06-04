a767203cf9ac97978461d2da4c71d9f88b74ef97
FacundoNavarro <facundonicolasnavarro@gmail.com>
2025-06-04 00:01:58 -0300

    delete RoutingDTO

---
+++ backend/src/main/java/com/example/tsigback/entities/dtos/ListaPuntosDTO.java
@@ -0,0 +1,16 @@
+package com.example.tsigback.entities.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListaPuntosDTO {
+    private List<PuntoDTO> points;
+}
